6461b2819e610619ae8f34d46b3a0a048e41629b
Artur Kowalski <arturkow2000@gmail.com>
2022-07-10 20:02:14 +0200

    USB: optimize FIFO
    Attempt to write/read 4 or 2 bytes at once to/from FIFO if possible.

--- Drivers/Usb/Otg/Common/Fifo.c
+++ Drivers/Usb/Otg/Common/Fifo.c
@@ -2,28 +2,64 @@
 
 VOID UsbReadFifo(USB_DRIVER *Driver, UINT8 Endpoint, UINT16 Length, UINT8* OutData) {
   UINTN Fifo;
-  UINT32 i;
+  UINT32 i = 0;
+  UINT16 Left;
 
   // TODO: validate endpoint
 
   Fifo = Driver->Base + Endpoint * 4;
 
-  // TODO: optimize this
-  for (i = 0; i < Length; i++) {
-    OutData[i] = MmioRead8(Fifo);
+  // Don't care about alignment, some components of EDK II don't handle
+  // alignment well on ARM so we have to keep Alignment Check off anyway.
+  Left = Length;
+  while (Left > 0) {
+    while (Left >= 4) {
+      *(UINT32*)&OutData[i] = MmioRead32(Fifo);
+      i += 4;
+      Left -= 4;
+    }
+
+    while (Left >= 2) {
+      *(UINT16*)&OutData[i] = MmioRead16(Fifo);
+      i += 2;
+      Left -= 2;
+    }
+
+    if (Left > 0) {
+      OutData[i] = MmioRead8(Fifo);
+      Left -= 1;
+    }
   }
 }
 
 VOID UsbWriteFifo(USB_DRIVER *Driver, UINT8 Endpoint, UINT16 Length, CONST UINT8* Data) {
   UINTN Fifo;
-  UINT32 i;
+  UINT32 i = 0;
+  UINT16 Left;
 
   // TODO: validate endpoint
 
   Fifo = Driver->Base + Endpoint * 4;
 
-  // TODO: optimize this
-  for (i = 0; i < Length; i++) {
-    MmioWrite8(Fifo, Data[i]);
+  // Don't care about alignment, some components of EDK II don't handle
+  // alignment well on ARM so we have to keep Alignment Check off anyway.
+  Left = Length;
+  while (Left > 0) {
+    while (Left >= 4) {
+      MmioWrite32(Fifo, *(UINT32*)&Data[i]);
+      i += 4;
+      Left -= 4;
+    }
+
+    while (Left >= 2) {
+      MmioWrite16(Fifo, *(UINT16*)&Data[i]);
+      i += 2;
+      Left -= 2;
+    }
+
+    if (Left > 0) {
+      MmioWrite8(Fifo, Data[i]);
+      Left -= 1;
+    }
   }
 }
